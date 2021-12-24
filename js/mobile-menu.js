3f54c1751a49b3589ee7e123814df5e4da078012
Voznyuk Dmitriy Leontevich <91959188+VoznyukDmitri@users.noreply.github.com>
2021-12-24 21:21:00 +0200

    фіксить відступи секції

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,13 @@
-const menuBtnRef = document.querySelector('[data-menu-button]');
-const menuNavRef = document.querySelector('[data-menu-nav]');
-// const menuHedRef = document.querySelector('[data-menu-hed]');
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const menuNavRef = document.querySelector('[data-menu-nav]');
+  // const menuHedRef = document.querySelector('[data-menu-hed]');
 
-menuBtnRef.addEventListener('click', () => {
-  menuBtnRef.classList.toggle('active');
-  menuNavRef.classList.toggle('active');
-  document.body.classList.toggle('modal-open');
-  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-  menuBtnRef.setAttribute('aria-expanded', !expanded);
-});
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('active');
+    menuNavRef.classList.toggle('active');
+    document.body.classList.toggle('modal-open');
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+  });
+})();
